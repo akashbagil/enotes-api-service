05e57919ef8692b03790bf4db5354bd7b1a987d6
Akash <akashbagil1999@gmail.com>
2025-07-24 19:48:42 +0530

    category dto mapper implementation

--- src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -3,10 +3,13 @@ package com.example.demo.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import com.example.demo.dto.CategoryDto;
+import com.example.demo.dto.CategoryResponse;
 import com.example.demo.entity.Category;
 import com.example.demo.repository.CategoryRespository;
 import com.example.demo.service.CategoryService;
@@ -16,9 +19,15 @@ public class CategoryServiceImpl implements CategoryService {
 
 	@Autowired
 	private CategoryRespository categoryRespository;
+	
+	@Autowired
+	private ModelMapper mapper;
 
 	@Override
-	public Boolean saveCategory(Category category) {
+	public Boolean saveCategory(CategoryDto categoryDto) {
+		
+		Category category = mapper.map(categoryDto, Category.class);
+		
 		category.setIsDeleted(false);
 		category.setCreatedBy(1);
 		category.setCreatedOn(new Date());
@@ -29,9 +38,20 @@ public class CategoryServiceImpl implements CategoryService {
 	}
 
 	@Override
-	public List<Category> getAllCategory() {
+	public List<CategoryDto> getAllCategory() {
 		List<Category> allCategory = categoryRespository.findAll();
-		return allCategory;
+		
+		List<CategoryDto> allCategoryDto = allCategory.stream().map(cat -> mapper.map(cat, CategoryDto.class)).toList();
+		return allCategoryDto;
+	}
+
+	@Override
+	public List<CategoryResponse> getAllActiveCategory() {
+		
+		List<Category> allCategory = categoryRespository.findByIsActiveTrue();
+		
+		List<CategoryResponse> categoryResponse = allCategory.stream().map(cat -> mapper.map(cat, CategoryResponse.class)).toList();
+		return categoryResponse;
 	}
 
 }
